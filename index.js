db548acaa280d784be23dbde89c037fea451ce21
Agostino <agostino.omare@student.moringaschool.com>
2025-03-06 11:54:24 +0300

    phase1 strings

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'GRACE HOPPER';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + 'GRACE HOPPER'+'!'
+const upperCaseCurrentUser = currentUser
+const shortGreeting = 'Welcome, ' + 'Grace Hopper' + '!'
+const firstInitial = currentUser
+const restOfName = currentUser 
+
+
+
+
+
